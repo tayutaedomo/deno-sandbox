d17166c046b9a8276a683578454e69cf2cb84a90
tayutaedomo <tayutaedomo@gmail.com>
2020-08-18 15:24:10 +0900

    Replace hello world with servest

--- main.ts
+++ main.ts
@@ -1,16 +1,39 @@
-import {
-  serve,
-} from "https://deno.land/std/http/mod.ts";
+/*
+ * std_http
+ */
+//import {
+//  serve,
+//} from "https://deno.land/std/http/mod.ts";
+//
+//const server = serve({ port: 8080 });
+//
+//for await (const req of server) {
+//  await req.respond({
+//    status: 200,
+//    headers: new Headers({
+//      "content-type": "text/plain",
+//    }),
+//    body: "hello deno!",
+//  });
+//}
 
-const server = serve({ port: 8080 });
 
-for await (const req of server) {
+/*
+ * Servest
+ */
+import {createApp} from "https://servestjs.org/@v1.1.2/mod.ts";
+
+const app = createApp();
+
+app.handle("/", async (req) => {
   await req.respond({
-    status: 200,
-    headers: new Headers({
-      "content-type": "text/plain",
-    }),
-    body: "hello deno!",
-  });
-}
+      status: 200,
+      headers: new Headers({
+            "content-type": "text/plain",
+          }),
+      body: "hello deno!",
+    });
+});
+
+app.listen({ port: 8080 });
 
